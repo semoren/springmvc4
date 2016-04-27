8b1975597adea253437859bc6c5de0da50f47e1d
rq <rq@192.168.1.105>
2016-04-28 00:03:05 +0800

    静态资源访问

--- src/main/java/com/sermo/springmvc4/MyMvcConfig.java
+++ src/main/java/com/sermo/springmvc4/MyMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -19,5 +20,14 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter{
 		viewResolver.setSuffix(".jsp");
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		/**
+		 * addResourceHandler 指的是文件放置的目录
+		 * addResourceLocations 指的是对外暴露的访问路径
+		 */
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assest/");
 	}
 }
